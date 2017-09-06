134e8cbc2a790f6003bce72dd76d68f678256236
Angelo Palmieri <palmieri.ang@gmail.com>
2017-09-06 17:20:17 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ class Board extends Component {
     console.log(i)
     const squares = this.state.squares.slice()
     squares[i] = this.state.xIsNext ? 'X' : 'O'
-    console.log(squares)
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext
@@ -89,6 +88,7 @@ class Game extends Component {
 
 export default Game;
 
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -114,102 +114,102 @@ let myMove = true
 
 function getWinner(board) {
    
-    // Check if someone won
-    var vals = [true, false];
-    var allNotNull = true;
-    for (var k = 0; k < vals.length; k++) {
-        var value = vals[k];
-        
-        // Check rows, columns, and diagonals
-        var diagonalComplete1 = true;
-        var diagonalComplete2 = true;
-        for (var i = 0; i < 3; i++) {
-            if (board[i][i] != value) {
-                diagonalComplete1 = false;
-            }
-            if (board[2 - i][i] != value) {
-                diagonalComplete2 = false;
-            }
-            var rowComplete = true;
-            var colComplete = true;
-            for (var j = 0; j < 3; j++) {
-                if (board[i][j] != value) {
-                    rowComplete = false;
-                }
-                if (board[j][i] != value) {
-                    colComplete = false;
-                }
-                if (board[i][j] == null) {
-                    allNotNull = false;
-                }
-            }
-            if (rowComplete || colComplete) {
-                return value ? 1 : 0;
-            }
+  // Check if someone won
+  var vals = [true, false];
+  var allNotNull = true;
+  for (var k = 0; k < vals.length; k++) {
+    var value = vals[k];
+    
+    // Check rows, columns, and diagonals
+    var diagonalComplete1 = true;
+    var diagonalComplete2 = true;
+    for (var i = 0; i < 3; i++) {
+      if (board[i][i] !== value) {
+        diagonalComplete1 = false;
+      }
+      if (board[2 - i][i] !== value) {
+        diagonalComplete2 = false;
+      }
+      var rowComplete = true;
+      var colComplete = true;
+      for (var j = 0; j < 3; j++) {
+        if (board[i][j] !== value) {
+          rowComplete = false;
         }
-        if (diagonalComplete1 || diagonalComplete2) {
-            return value ? 1 : 0;
-            myMove = false;
+        if (board[j][i] !== value) {
+          colComplete = false;
         }
+        if (board[i][j] == null) {
+          allNotNull = false;
+        }
+      }
+      if (rowComplete || colComplete) {
+        return value ? 1 : 0;
+      }
     }
-    if (allNotNull) {
-        return -1;
+    if (diagonalComplete1 || diagonalComplete2) {
+        return value ? 1 : 0;
+        myMove = false;
     }
-    return null;
+  }
+  if (allNotNull) {
+    return -1;
+  }
+  return null;
 }
 
 function makeMove(board) {
-    setTimeout(function() {
-        board = minimaxMove(board);
-        console.log(numNodes);
-        console.log('selected');
-        console.log(board);
-        myMove = false;
-    }, 800);
+  setTimeout(function() {
+    board = minimaxMove(board);
+    console.log(numNodes);
+    console.log('selected');
+    console.log(board);
+    myMove = false;
+  }, 800);
 }
 
 function minimaxMove(board) {
-    numNodes = 0;
-    return recurseMinimax(board, true)[1];
+  numNodes = 0;
+  return recurseMinimax(board, true)[1];
 }
 
 var numNodes = 0;
 
 function recurseMinimax(board, player) {
-    numNodes++;
-    var winner = calculateWinner(board);
-    if (winner != null) {
-        switch(winner) {
-            case 1:
-                // AI wins
-                return [1, board]
-            case 0:
-                // opponent wins
-                return [-1, board]
-            case -1:
-                // Tie
-                return [0, board];
-        }
-    } else {
-        // Next states
-        var nextVal = null;
-        var nextBoard = null;
-        
-        for (var i = 0; i < 3; i++) {
-            for (var j = 0; j < 3; j++) {
-                if (board[i][j] == null) {
-                    board[i][j] = player;
-                    var value = recurseMinimax(board, !player)[0];
-                    if ((player && (nextVal == null || value > nextVal)) || (!player && (nextVal == null || value < nextVal))) {
-                        nextBoard = board.map(function(arr) {
-                            return arr.slice();
-                        });
-                        nextVal = value;
-                    }
-                    board[i][j] = null;
-                }
-            }
-        }
-        return [nextVal, nextBoard];
+  numNodes++;
+  var winner = calculateWinner(board);
+  if (winner != null) {
+    switch(winner) {
+      case 1:
+        // AI wins
+        return [1, board]
+      case 0:
+        // opponent wins
+        return [-1, board]
+      case -1:
+        // Tie
+        return [0, board];
     }
+  } else {
+    // Next states
+    var nextVal = null;
+    var nextBoard = null;
+    
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        if (board[i][j] == null) {
+          board[i][j] = player;
+          var value = recurseMinimax(board, !player)[0];
+          if ((player && (nextVal == null || value > nextVal)) || (!player && (nextVal == null || value < nextVal))) {
+            nextBoard = board.map(function(arr) {
+              return arr.slice();
+            });
+            nextVal = value;
+          }
+          board[i][j] = null;
+        }
+      }
+    }
+    return [nextVal, nextBoard];
+  }
 }
